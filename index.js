fd56601841f04f731264fad77a0f671d11f7cf05
Ben Parnell <benjaminparnell.94@gmail.com>
2015-04-02 00:24:42 +0100

    add commander for more options and better help

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 #! /usr/bin/env node
 
+var program = require('commander')
 var tr = require('trumpet')()
-  , minimist = require('minimist')
-  , argv = minimist(process.argv.slice(2))
+var split = require('split')
+var fs = require('fs')
+var inputStream
 
-tr.selectAll(argv._[0], function (result) {
-  result.createReadStream().pipe(process.stdout)
-})
+program
+  .version(require('./package.json').version)
+  .usage('selector [file]')
+  .option('-f, --first', 'only return the first matched element')
+  .parse(process.argv)
 
-process.stdin.resume()
-process.stdin.pipe(tr)
+if (program.args.length > 1) {
+  inputStream = fs.createReadStream(program.args[1])
+} else {
+  inputStream = process.stdin
+  inputStream.resume()
+}
+
+if (program.first) {
+  tr.select(program.args[0]).createReadStream().pipe(process.stdout)
+} else {
+  tr.selectAll(program.args[0], function (result) {
+    result.createReadStream().pipe(split(null, mapper)).pipe(process.stdout)
+  })
+}
+
+inputStream.pipe(tr)
+
+function mapper (string) {
+  return string + '\n\n'
+}
